41da7444a51c1070cb4e0cd988b85a4910c89371
qkr10 <qkrtngks10@naver.com>
2025-01-14 15:54:46 +0900

    header 파일 분리

---
+++ header.js
@@ -0,0 +1,22 @@
+let header = `<div class="header-buttons">
+    <button class="btn red">
+        <a href="member.html">인원 입력</a>
+    </button>
+    <button class="btn orange">
+        <a href="schedule.html">일과시간 지정</a>
+    </button>
+    <button class="btn orange">
+        <a href="memberCheck.html">인원 배정</a>
+    </button>
+    <button class="btn blue">
+        <a href="work.html">작업지시</a>
+    </button>
+    <button class="btn green">
+        <a href="download.html">내려받기</a>
+    </button>
+    <button class="btn black">
+        <a href="manual.html">설명서</a>
+    </button>
+</div>`;
+
+document.writeln(header);
