bb0fb3d9094fc0ba04bd0878be6413e8786960f2
qkr10 <qkrtngks10@naver.com>
2025-01-14 21:10:04 +0900

    엑셀파일 다운로드 하는 코드 추가

--- makeCellFile.js
+++ makeCellFile.js
@@ -1,67 +1,34 @@
-//<script src="jszip.min.js"></script> 를 같이 추가해야함
+// 이 파일을 사용할때는 다른 라이브러리도 같이 추가해 줘야함.
+// <script src="FileSaver.min.js"></script>
+// <script src="xlsx.mini.min.js"></script>
+// <script src="makeCellFile.js"></script>
 
 //table = 이차원 배열, filename = 파일명.확장자
 function makeCellFile(table, filename) {
-    const content = table.map((innerArr, index) => {
-        const row = index + 1;
-        const innerContent = innerArr.map((value, i) => {
-            const col = String.fromCharCode('A'.charCodeAt(0) + index);
-            return `<c r="${col}${row}" t="inlineStr"><is><t>${value}</t></is></c>`;
-        });
-        const content = `
-        <row r="${row}">
-            ${innerContent}
-        </row>
-        `;
-        return content;
-    });
+    // workbook 생성
+    let wb = XLSX.utils.book_new();
 
-    const workbookXml = `<?xml version="1.0" encoding="UTF-8"?>
-        <workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-            <sheets>
-                <sheet name="Sheet1" sheetId="1" r:id="rId1"/>
-            </sheets>
-        </workbook>`;
+    // sheet 생성
+    wb.SheetNames.push("sheet 1");
 
-    let temp = `<?xml version="1.0" encoding="UTF-8"?>
-        <worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-            <sheetData>`;
-    temp += '\n' + content;
-    const worksheetXml = temp + `
-            </sheetData>
-        </worksheet>`;
+    // 배열 데이터로 시트 데이터 생성
+    let ws = XLSX.utils.aoa_to_sheet(table);
 
-    const relsXml = `<?xml version="1.0" encoding="UTF-8"?>
-        <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
-            <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/>
-        </Relationships>`;
+    // 시트 데이터를 시트에 넣기 ( 시트 명이 없는 시트인경우 첫번째 시트에 데이터가 들어감 )
+    wb.Sheets["sheet 1"] = ws;
 
-    const contentTypesXml = `<?xml version="1.0" encoding="UTF-8"?>
-        <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
-            <Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/>
-            <Default Extension="xml" ContentType="application/xml"/>
-            <Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/>
-            <Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/>
-        </Types>`;
+    // 엑셀 파일 쓰기
+    let wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
 
-    const zip = new JSZip();
-
-    // Add files to the ZIP
-    zip.file("[Content_Types].xml", contentTypesXml);
-    zip.file("_rels/.rels", relsXml);
-    zip.file("xl/workbook.xml", workbookXml);
-    zip.file("xl/worksheets/sheet1.xml", worksheetXml);
-
-    // Generate ZIP file and create Blob
-    zip.generateAsync({ type: "blob" }).then(function (content) {
-        // Create a download link
-        const link = document.createElement("a");
-        link.href = URL.createObjectURL(content);
-        link.download = filename;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    });
+    // ArrayBuffer 만들어주는 함수
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+        var view = new Uint8Array(buf);  //create uint8array as viewer
+        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+        return buf;
+    }
+    // 파일 다운로드
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), '엑셀_다운로드.xlsx');
 }
 
 function makeTestCellFile() {
